22bd76e12a13852ebfe90707bc0522e27debb66d
NVeena30919 <nerendlaveena@gmail.com>
2020-02-08 23:49:40 +0530

    Task 2

---
+++ assignment2/src/main/java/epam/assignment2/Gift.java
@@ -0,0 +1,9 @@
+package epam.assignment2;
+
+public abstract class Gift 
+{
+	public abstract int total_weight(int a[],int n);
+	
+	public abstract int num_of_candy(int low,int high,int a[],int n);
+
+}
